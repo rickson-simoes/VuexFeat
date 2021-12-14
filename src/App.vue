de3f96ffe311963e840de60a1f1078d7899841aa
Rickson <rickson.simoes@hotmail.com>
2021-12-14 13:57:08 -0300

    :wrench: Fixing issues - :sparkles:

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div id="nav">
       <router-link :to="{ name: 'EventList' }">Events</router-link> |
       <router-link :to="{ name: 'About' }">About</router-link>
-       |
+      |
       <router-link :to="{ name: 'EventCreate' }">Create Event</router-link>
     </div>
     <router-view />
@@ -18,7 +18,6 @@
   text-align: center;
   color: #2c3e50;
 }
-
 button {
   align-items: center;
   height: 50px;
@@ -32,59 +31,46 @@ button {
   cursor: pointer;
   transition: all 0.2s linear;
 }
-
 button:hover {
   -webkit-transform: scale(1.02);
   transform: scale(1.01);
   box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
 }
-
 h2 {
   font-size: 40px;
   font-weight: 700;
 }
-
 h3 {
   font-size: 25px;
   font-weight: 700;
 }
-
 h4 {
   font-size: 20px;
   font-weight: 700;
 }
-
 input {
   display: block;
   width: 100%;
   height: 30px;
   margin-bottom: 24px;
 }
-
-input[type=text] {
+input[type='text'] {
   padding: 0px 10px;
 }
-
 input:focus {
   border-color: #16c0b0;
   outline: 0;
 }
-
-
 #nav {
   padding: 30px;
 }
-
 #nav a {
   font-weight: bold;
   color: #2c3e50;
 }
-
 #nav a.router-link-exact-active {
   color: #42b983;
 }
-
-
 .form-container {
   display: flex;
   justify-content: center;
@@ -92,7 +78,6 @@ input:focus {
   align-items: center;
   margin-bottom: 10%;
 }
-
 select:focus {
   border-color: #16c0b0;
   outline: 0;
